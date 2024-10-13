b2df8bd5546e5ba1e6bd8b902bfccc1112cbdc88
설승환 <tjftmd7@gmail.com>
2024-10-13 19:07:08 +0900

    BOJ(DP): 9251번 LCS

---
+++ src/bj/dp/BJ9251.java
@@ -0,0 +1,32 @@
+package bj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+LCS 문제
+ */
+public class BJ9251 {
+    static int[][] lcs;
+    static char[] first;
+    static char[] second;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        first = br.readLine().toCharArray();
+        second = br.readLine().toCharArray();
+        lcs = new int[first.length+ 1][second.length + 1];
+        for (int i = 1; i <= first.length; i++) {
+            for (int j = 1; j <= second.length; j++) {
+                if(first[i-1] == second[j-1]){
+                    lcs[i][j] = lcs[i-1][j-1]+ 1;
+                    // i-1, j-1인 이유는 그래야 [i][j]번 째 들어가는 요소가 개입이 안되기 때문
+                }
+                else{
+                    lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+                }
+            }
+        }
+        System.out.println(lcs[first.length][second.length]);
+    }
+}
